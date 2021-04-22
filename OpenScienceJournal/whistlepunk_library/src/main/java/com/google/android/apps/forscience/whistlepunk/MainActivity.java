5095f745fff21f10b33428422e58106f82e3371b
Carlo Pelliccia <>
2021-04-22 16:14:47 +0200

    gdrive sync wip

--- OpenScienceJournal/whistlepunk_library/src/main/java/com/google/android/apps/forscience/whistlepunk/MainActivity.java
+++ OpenScienceJournal/whistlepunk_library/src/main/java/com/google/android/apps/forscience/whistlepunk/MainActivity.java
@@ -630,7 +630,10 @@ public class MainActivity extends ActivityWithNavigationView {
         if (appAccount instanceof NonSignedInAccount) {
             avatarView.setImageResource(R.drawable.ic_navigation_user_avatar);
             nicknameView.setText(R.string.arduino_auth_sign_in_action);
-            navigationView.getMenu().findItem(R.id.navigation_item_settings).setVisible(false);
+            final Menu menu = navigationView.getMenu();
+            menu.findItem(R.id.navigation_item_get_science_kit).setVisible(true);
+            menu.findItem(R.id.navigation_item_get_help).setVisible(true);
+            menu.findItem(R.id.navigation_item_settings).setVisible(false);
         } else {
             final String avatar = appAccount.getAccountAvatar();
             if (StringUtils.isEmpty(avatar)) {
@@ -662,7 +665,10 @@ public class MainActivity extends ActivityWithNavigationView {
                 }
             }
             nicknameView.setText(StringUtils.emptyIfNull(appAccount.getAccountName()));
-            navigationView.getMenu().findItem(R.id.navigation_item_settings).setVisible(true);
+            final Menu menu = navigationView.getMenu();
+            menu.findItem(R.id.navigation_item_get_science_kit).setVisible(!appAccount.isMinor());
+            menu.findItem(R.id.navigation_item_get_help).setVisible(!appAccount.isMinor());
+            menu.findItem(R.id.navigation_item_settings).setVisible(true);
         }
 
         // Clean up old files from previous exports.
